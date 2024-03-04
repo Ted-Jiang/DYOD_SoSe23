c103a6cf5740a5b6830d39c18e8abfa58a598ef7
yangjiang <yangjiang@ebay.com>
2024-03-04 14:33:40 +0800

    [Sprint 1]implement value segment

--- src/lib/storage/value_segment.cpp
+++ src/lib/storage/value_segment.cpp
@@ -6,68 +6,75 @@
 namespace opossum {
 
 template <typename T>
-ValueSegment<T>::ValueSegment(bool nullable) {
-  // Implementation goes here
-}
+ValueSegment<T>::ValueSegment(bool nullable) : _values{}, _is_null_values{}, _segment_is_nullable(nullable) {}
 
 template <typename T>
 AllTypeVariant ValueSegment<T>::operator[](const ChunkOffset chunk_offset) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  if (is_null(chunk_offset)) {
+    return NULL_VALUE;
+  }
+  return get(chunk_offset);
 }
 
 template <typename T>
 bool ValueSegment<T>::is_null(const ChunkOffset chunk_offset) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  return is_nullable() && _is_null_values.at(chunk_offset);
 }
 
 template <typename T>
 T ValueSegment<T>::get(const ChunkOffset chunk_offset) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  Assert(!is_null(chunk_offset), "Chunk is null, can't return value.");
+  return _values[chunk_offset];
 }
 
 template <typename T>
 std::optional<T> ValueSegment<T>::get_typed_value(const ChunkOffset chunk_offset) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  if (is_null(chunk_offset)) {
+    return std::nullopt;
+  }
+  return get(chunk_offset);
 }
 
 template <typename T>
 void ValueSegment<T>::append(const AllTypeVariant& value) {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  if (variant_is_null(value)) {
+    Assert(_segment_is_nullable, "Tried to insert NULL value in not nullable segment!");
+    _values.push_back(type_cast<T>(0));
+    _is_null_values.push_back(true);
+  } else {
+    try {
+      _values.push_back(type_cast<T>(value));
+      _is_null_values.push_back(false);
+    } catch (...) {
+      throw std::logic_error{"Wrong argument type in append"};
+    }
+  }
 }
 
 template <typename T>
 ChunkOffset ValueSegment<T>::size() const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  return _values.size();
 }
 
 template <typename T>
 const std::vector<T>& ValueSegment<T>::values() const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  return _values;
 }
 
 template <typename T>
 bool ValueSegment<T>::is_nullable() const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  return _segment_is_nullable;
 }
 
 template <typename T>
 const std::vector<bool>& ValueSegment<T>::null_values() const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  Assert(is_nullable(), "NULL values are only available if the segment is nullable.");
+  return _is_null_values;
 }
 
 template <typename T>
 size_t ValueSegment<T>::estimate_memory_usage() const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  return _values.size() * sizeof(T);
 }
 
 // Macro to instantiate the following classes:
